1edb32e478b002caaf4f9d136c4ee267d5e43a01
JanjyTapYT <81481526+JanjyTapYT@users.noreply.github.com>
2022-06-08 16:18:31 +0200

    Update award.team.js

--- award.team.js
+++ award.team.js
@@ -4,9 +4,13 @@ module.exports = [
         role: "Founder",
     },
     {
+        id: "964509885658169344",
+        role: "Developer",
+    },
+    {
         id: "897829235194998835",
         role: "Developer",
-    },    
+    },
     {
         id: "530832812274614303",
         role: "Community Manager",
